08a9110657e416195a25391897911af695f2b254
Bruhsondagoat <110129034+Bruhsondagoat@users.noreply.github.com>
2022-08-05 13:19:55 -0500

    Update sketch.js
    here is my code

--- sketch.js
+++ sketch.js
@@ -9,17 +9,40 @@ let bulletDirection = 1;
 let enemyYBullet = 40;
 let enemyXBullet = 250;
 let gameOver = false
+
 function setup() {
     createCanvas(500, 750);
     noStroke();
     rectMode(CENTER);
  }
+
+ function start() {
+   background(0);
+    if (keyPressed == ENTER){
+    draw();
+ }
+}
+
  function draw() {
-    background(0);
+   game();
+
+   enemy();
+   keyPressed();
+   flying();
+   myHitBox();
+   enemyHitBox();
+   theEnd();
+}
+
+
+function game(){
+background(0);
     fill(0, 255, 0);
     rect(bulletX, bulletY, 6, 14);
+
     fill(255, 0, 0);
     rect(myXPos, myYPos, 40, 40);
+
     if (keyIsDown(LEFT_ARROW)) {
         myXPos -=3;
         if (bulletY == 700) {
@@ -40,92 +63,113 @@ if (myXPos < 20) {
     myXPos = 479;
     bulletX = 479;
  }
+
  fill(255, 164, 0)
  rect(enemyXBullet, enemyYBullet, 10, 22)
  fill(0, 0, 255)
  rect(enemyXPos, enemyYPos, 34, 34)
- enemyXPos += 3 * direction;
-enemyXBullet += 3 * bulletDirection;
- if (enemyXPos > 475 || enemyXPos < 25 ){
+
+ if (enemyXPos > 476) {
+   enemyXPos = 475;
+ }
+ if (enemyXPos < 24){
+   enemyXPos = 25;
+ }
+
+ let speed = random([6, 10])
+ enemyXPos += speed * direction;
+enemyXBullet += speed * bulletDirection;
+
+ if (enemyXPos > 476 || enemyXPos < 24 ){
    direction *= -1;
    bulletDirection *= -1;
 }
-enemyShoot();
-enemyFlying();
-keyPressed();
-flying();
-myHitBox();
-enemyHitBox();
-theEnd();
+
 if (bulletY < -8) {
    bulletY = myYPos;
    bulletX = myXPos;
 }
 }
+
 function flying() {
    if (bulletY < 700) {
-      bulletY -=5;
+      bulletY -=8;
    }
 }
+
 function keyPressed() {
    if (keyCode === UP_ARROW) {
       bulletY -= 1;
 }
 }
-function enemyShoot(){
+
+function enemy(){
    if (enemyYBullet > 40) {
       bulletDirection *= 0;
    }
+   
    if (enemyYBullet > 800) {
       enemyYBullet = 40;
       enemyXBullet = enemyXPos;
    }
    enemyYBullet += 1;
-}
-function enemyFlying() {
+
    if (enemyYBullet > 40) {
-      enemyYBullet +=6;
+      enemyYBullet +=9;
    }
 }
+
 function myHitBox() {
 myLeft = myXPos - 20;
 myRight = myXPos + 20;
 myTop = myYPos - 20;
 myBottom = myYPos + 20;
+
 bulletLeft = enemyXBullet - 5;
 bulletRight = enemyXBullet + 5;
 bulletTop = enemyYBullet - 11;
 bulletBottom = enemyYBullet + 11;
+
 if (myLeft > bulletRight || myRight < bulletLeft || myTop > bulletBottom || myBottom < bulletTop) {
+
 }
+
 else {
    background(0)
+
     fill(255, 255, 255);
     textSize(100);
     text("L", 250, 350);
     gameOver = true
    }
 }
+
 function enemyHitBox() {
    myBulletLeft = bulletX - 3;
    myBulletRight = bulletX + 3;
    myBulletTop = bulletY - 7;
    myBulletBottom = bulletY + 7;
+   
    enemyLeft = enemyXPos - 17;
    enemyRight = enemyXPos + 17;
    enemyTop = enemyYPos - 17;
    enemyBottom = enemyYPos + 17;
+   
    if (myBulletLeft > enemyRight || myBulletRight < enemyLeft || myBulletTop > enemyBottom || myBulletBottom < enemyTop) {
+   
    }
+   
    else {
       background(0)
+   
        fill(255, 255, 255);
        textSize(80);
        text("You won!", 100, 350);
        gameOver = true
       }
  }
+
 function theEnd() {
    if (gameOver == true)
-   endLoop();
+   noLoop();
 }
